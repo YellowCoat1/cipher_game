853f5dc60ba0798e257acf694bcec3cf5a99631c
Jason DeFillipo <jasond3663@gmail.com>
2026-09-11 21:21:24 -0700

    screenManager auto callback registration

--- main.lua
+++ main.lua
@@ -2,8 +2,6 @@ package.path = "./?.lua" .. ';./' .. "libs" .. '/?.lua'
 
 ScreenManager = require("libs.ScreenManager")
 
-local dialog
-
 function love.load()
 
 	local screens = {
@@ -13,26 +11,10 @@ function love.load()
 		dialogue = require ('screens.dialogue')
 	}
 
+	ScreenManager.registerCallbacks()
 	ScreenManager.init(screens, 'manager')
 	ScreenManager.push("scrollingBG")
 	ScreenManager.push("main")
 	love.graphics.setBackgroundColor(1, 1, 1, 1)
 end
 
-function love.update(dt)
-	ScreenManager.update(dt)
-	if dialog then dialog:update(dt) end
-end
-
-function love.draw()
-	ScreenManager.draw()
-end
-
-
-function love.keypressed(key, scancode, isrepeat)
-	ScreenManager.keypressed(key, scancode, isrepeat)
-end
-
-function love.mousereleased(x, y, button)
-	ScreenManager.mousereleased(x, y, button)
-end

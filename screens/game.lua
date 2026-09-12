f8655be89d5a717b85583da02b4b6e480ef962e9
Jason DeFillipo <jasond3663@gmail.com>
2026-09-11 22:33:10 -0700

    basic node

--- screens/game.lua
+++ screens/game.lua
@@ -2,9 +2,43 @@ local Screen = require('screens.Screen')
 
 local game = {}
 
-function game.new()
-	self = Screen:new()
+local ring_png = love.graphics.newImage("assets/ring.png")
+local ring_width, ring_height = ring_png:getWidth(), ring_png:getHeight()
 
+local function node(x, y)
+	local node = {}
+	node.x = x or 100
+	node.y = y or 100
+	node.ring1_rotation = 0
+
+	function node:draw()
+		love.graphics.setColor(0.3, 0.3, 0.3)
+		love.graphics.circle("fill", self.x, self.y, 30)
+		local ring_scale = 2
+		love.graphics.draw(ring_png, self.x, self.y, self.ring1_rotation, ring_scale, ring_scale, ring_width/2, ring_height/2)
+	end
+
+	function node:update(dt)
+		self.ring1_rotation = self.ring1_rotation + 5*dt
+	end
+
+	return node
+end
+
+function game.new()
+	local self = Screen:new()
+	
+	local node1 = node(200, 200)
+
+	function self:draw()
+		love.graphics.push()
+		node1:draw()
+		love.graphics.pop()
+	end
+
+	function self:update(dt)
+		node1:update(dt)
+	end
 	return self
 end
 

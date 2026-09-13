e5f938c723dd367e4c748199dcb001ce01f58e06
Jason DeFillipo <jasond3663@gmail.com>
2026-09-12 23:17:15 -0700

    rising sea in the tutorial

--- screens/tutorial.lua
+++ screens/tutorial.lua
@@ -10,6 +10,11 @@ local phases = {
 	AFTER_SEA = 3,
 }
 
+local function smoothLerp(start, endt, t)
+  t = 1 - math.pow(1 - t, 3)
+  return Lerp(start, endt, t)
+end
+
 function tutorial.new()
 	local self = Screen:new()
 	self.phase = phases.BEFORE_NODE
@@ -19,10 +24,14 @@ function tutorial.new()
 			self:tut1End()
 		end
 	end
+	local sea = require("sea")
+	sea.ylevel = love.graphics.getHeight() + 50
+	self.rising_sea_timer = nil
 
 	function self:draw()
 		if self.dialog_scene then self.dialog_scene:draw() end
 		if self.node then self.node:draw() end
+		sea:draw()
 	end
 	function self:update(dt)
 		if self.dialog_scene then self.dialog_scene:update(dt) end
@@ -33,6 +42,17 @@ function tutorial.new()
 				self:tut2Start()
 			end
 		end
+		sea:update(dt)
+		if self.rising_sea_timer then
+			self.rising_sea_timer = self.rising_sea_timer + (1/5)*dt
+			local min_timer = math.min(self.rising_sea_timer, 1)
+			sea.ylevel = smoothLerp(love.graphics.getHeight()+10, love.graphics.getHeight()-200, min_timer)
+
+			--if self.rising_sea_timer > 1 and self.rising_sea_timer < 100 then
+			--	self:tut3Start()
+			--end
+		end
+
 	end
 	function self:keypressed(key)
 		if self.dialog_scene then self.dialog_scene:keypressed(key) end
@@ -48,7 +68,19 @@ function tutorial.new()
 
 	function self:tut2Start()
 		self.dialog_scene = loveDialogue.play("scripts/tut2.ld", DialogueConfig)
+		local tut = self
+		self.dialog_scene.onSignal = function(name, _)
+			if name == "Tut2End" then
+				tut.rising_sea_timer = 0
+				sea.ylevel = love.graphics.getHeight() + 10
+				self.rising_sea_timer = 0
+			end
+		end
 	end
+
+	--function self:tut3Start()
+	--	self.dialog_scene = loveDialogue.play("scripts/tut3.ld", DialogueConfig)
+	--end
 
 
 	return self
--- sea.lua
+++ sea.lua
@@ -3,7 +3,7 @@ local offset = 0
 
 local line_len = 30
 local space_len = 10
-local ylevel = love.graphics.getHeight()-50
+sea.ylevel = love.graphics.getHeight()-50
 
 local negative_primary = {148/255,102/255,1/255}
 local negative_secondary = {238/255,226/255,0/255}
@@ -12,10 +12,10 @@ function sea:draw()
 	love.graphics.setColor(negative_primary)
 	local total_lines = love.graphics.getWidth()/(line_len + space_len)
 	for i=-3,total_lines+3,1 do
-		love.graphics.line(i*(line_len+space_len)+offset, ylevel, i*(line_len+space_len)+line_len+offset, ylevel)
+		love.graphics.line(i*(line_len+space_len)+offset, self.ylevel, i*(line_len+space_len)+line_len+offset, self.ylevel)
 	end
 	love.graphics.setColor(negative_secondary[1], negative_secondary[2], negative_secondary[3], 0.4)
-	love.graphics.rectangle("fill", 0, ylevel, love.graphics.getWidth(), 10+love.graphics.getHeight()-ylevel)
+	love.graphics.rectangle("fill", 0, self.ylevel, love.graphics.getWidth(), 10+love.graphics.getHeight()-self.ylevel)
 end
 
 function sea:update(dt)

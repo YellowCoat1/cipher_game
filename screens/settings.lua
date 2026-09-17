a47fa34ea4ad8a5aea7018e120c69b656c9e19ff
Jason DeFillipo <jasond3663@gmail.com>
2026-09-17 01:31:43 -0700

    non-functional main volume slider

--- screens/settings.lua
+++ screens/settings.lua
@@ -1,15 +1,20 @@
 local screen = require 'screens.Screen'
 local colors = require 'colors'
 local button = require 'button'
+local slider = require 'slider'
 
 local coal_color = colors.coal_color
 local settings = {}
 
 local line = love.graphics.line
 
+local font = love.graphics.newFont(FontName, 24)
+
 function settings.new()
 	local self = screen:new()
 	local exit_button = button.new(1, 1, 1, 1)
+	local volume_slider =  slider.new(1, 1, 1, 1, 0.5)
+
 
 	function self:exit_button_calc()
 		local width, height = love.graphics.getDimensions()
@@ -17,6 +22,13 @@ function settings.new()
 		exit_button.y = height*(4/5) - 80
 		exit_button.width = 50
 		exit_button.height = 50
+	end
+	function self:sliders_calc()
+		local width, height = love.graphics.getDimensions()
+		volume_slider.x = width*(1/5) + 50
+		volume_slider.y = height*(1/5) + 50
+		volume_slider.width = 300
+		volume_slider.height = 40
 	end
 
 	function exit_button.draw()
@@ -35,6 +47,8 @@ function settings.new()
 	end
 
 	self:exit_button_calc()
+	self:sliders_calc()
+	love.graphics.setColor(0, 0, 0, 1)
 
 	function self:draw()
 		love.graphics.setColor(colors.with_opacity(coal_color, 0.95))
@@ -51,10 +65,16 @@ function settings.new()
 		love.graphics.setLineWidth(1)
 		--love.graphics.print("settings", love.graphics.getWidth()/2, love.graphics.getHeight()/2)
 		exit_button:draw()
+		volume_slider:draw()
+		local y_offset = (volume_slider.height-font:getHeight())*(1/2)
+		love.graphics.print("Main Volume", font, volume_slider.x + volume_slider.width + 20, volume_slider.y+y_offset)
 	end
 
 	function self:update()
+		volume_slider:update()
 
+		self:exit_button_calc()
+		self:sliders_calc()
 	end
 
 	function self:keypressed()
@@ -67,10 +87,12 @@ function settings.new()
 
 	function self:mousepressed(x, y, m)
 		exit_button:mousepressed(x, y, m)
+		volume_slider:mousepressed(x, y, m)
 	end
 
 	function self:mousereleased(x, y, m)
 		exit_button:mousereleased(x, y, m)
+		volume_slider:mousereleased(x, y, m)
 	end
 
 	return self

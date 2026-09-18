97da01ac8de0718d2a5853bd332b6214abc73d43
Jason DeFillipo <jasond3663@gmail.com>
2026-09-17 21:39:43 -0700

    window name and icon

--- conf.lua
+++ conf.lua
@@ -1,4 +1,8 @@
 function love.conf(t)
+	t.identity = "cipher game"
+	t.version = "11.5"
 	t.window.height = 900
 	t.window.width = 1200
+	t.window.title = "cipher game"
+	t.window.title = "assets/cipher.png"
 end

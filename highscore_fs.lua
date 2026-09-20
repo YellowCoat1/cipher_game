abc1cfc862b5f4a2a40e1b1209a490cda5821a11
Jason DeFillipo <jasond3663@gmail.com>
2026-09-19 19:35:53 -0700

    gutted fs operations

--- highscore_fs.lua
+++ highscore_fs.lua
@@ -1,19 +1,13 @@
 local highscore_fs = {}
 
 function highscore_fs.get()
-	local info = love.filesystem.read("highscore")
-	return info
 end
 function highscore_fs.get_spike()
-	local info = love.filesystem.read("highscore_spikes")
-	return info
 end
 
-function highscore_fs.set(hs)
-	love.filesystem.write("highscore", tostring(hs))
+function highscore_fs.set()
 end
-function highscore_fs.set_spike(hs)
-	love.filesystem.write("highscore_spikes", tostring(hs))
+function highscore_fs.set_spike()
 end
 
 return highscore_fs

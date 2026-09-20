d77e4977a1e4439a22f11e75e92114dbe69093ef
Jason DeFillipo <jasond3663@gmail.com>
2026-09-19 19:18:04 -0700

    optimized hex grid

--- screens/scrollingBG.lua
+++ screens/scrollingBG.lua
@@ -5,33 +5,21 @@ local sideLength = 20
 local sin60 = math.sin(math.rad(60))
 local cos60 = math.cos(math.rad(60))
 
-local function drawHexChunk(x, y)
-	local x1, y1 = x-(sideLength*sin60), y-(sideLength*cos60)
-	local x2, y2 = x1-(sideLength*sin60), y1+(sideLength*cos60)
-	local x3, y3 = x2, y2+sideLength
-	love.graphics.line(x, y, x1, y1, x2, y2, x3, y3)
-	return x2, y2
-end
+local hex_grid
+local canvas
 
-local function drawHexLine(x, y, n)
-	local lineX, lineY = x, y
-	for _ = 1, n, 1 do
-		lineX, lineY = drawHexChunk(lineX, lineY)
-	end
+local function chunk(x, y)
+	x = math.floor(x)
+	y = math.floor(y)
+	love.graphics.draw(hex_grid, x, y, 0, 1/2, 1/2)
 end
-
-local function drawHexGrid(x, y, w, h)
-	local columnX, columnY = x, y
-	local offset_right = false
-	for _=1,h do
-		drawHexLine(columnX, columnY, w)
-		columnX, columnY = columnX, columnY+sideLength
-		if not offset_right then
-			columnX, columnY = columnX-sideLength*sin60, columnY+sideLength*cos60
-			offset_right = not offset_right
-		else
-			columnX, columnY = columnX+sideLength*sin60, columnY+sideLength*cos60
-			offset_right = not offset_right
+local function drawHexGrid(xOffset, yOffset)
+	local hex_height, hex_width = hex_grid:getHeight()/2, hex_grid:getWidth()/2 - 1
+	local chunks_across =  love.graphics.getWidth() / hex_width
+	local chunks_below =  love.graphics.getHeight() / hex_height
+	for i=0,chunks_across do
+		for j=0,chunks_below do
+			chunk(-50 + xOffset + i*hex_width, -50 + yOffset + j*hex_height)
 		end
 	end
 end
@@ -42,12 +30,19 @@ function scrollingBackground.new()
 	self.xOffset = 0
 	self.yOffset = 0
 
+	hex_grid = love.graphics.newImage('assets/hex.png')
+	canvas = love.graphics.newCanvas()
+
 	function self:draw()
 		love.graphics.push()
+		love.graphics.setCanvas(canvas)
+		love.graphics.clear()
 		love.graphics.setColor(0.75, 0.75, 0.8, 1)
 		love.graphics.setLineWidth(1)
-		local width, height = love.graphics.getWidth(), love.graphics.getHeight()
-		drawHexGrid(width+5*sideLength+self.xOffset, self.yOffset-100, 5+width/(2*sideLength*cos60), 5+height/(2*sideLength*cos60))
+		love.graphics.setDefaultFilter("nearest", "nearest")
+		drawHexGrid(self.xOffset, self.yOffset)
+		love.graphics.setCanvas()
+		love.graphics.draw(canvas, 0, 0)
 		love.graphics.pop()
 	end
 
